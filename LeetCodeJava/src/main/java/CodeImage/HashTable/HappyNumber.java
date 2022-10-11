a2a85fba80a6c4085f39141ed323872f6cc2ef65
TomaChen513 <xy_chen513@163.com>
2022-10-11 09:32:32 +0800

    feat(LeetCodeJava): add 202. Happy Number

--- LeetCodeJava/src/main/java/CodeImage/HashTable/HappyNumber.java
+++ LeetCodeJava/src/main/java/CodeImage/HashTable/HappyNumber.java
@@ -1,5 +1,31 @@
 package CodeImage.HashTable;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HappyNumber {
-    1
+    public boolean isHappy(int n){
+        Set<Integer> set=new HashSet<>();
+        set.add(n);
+        while (true){
+            if (n==1){
+                return true;
+            }
+            n=getSum(n);
+            if (set.contains(n)){
+                return false;
+            }else {
+                set.add(n);
+            }
+        }
+    }
+    public int getSum(int n){
+        int res=0;
+        while (n!=0){
+            int temp=n%10;
+            n=n/10;
+            res+=temp*temp;
+        }
+        return res;
+    }
 }
