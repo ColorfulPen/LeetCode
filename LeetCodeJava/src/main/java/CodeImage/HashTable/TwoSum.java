e6bac6ce5b57d6d15a9c1fbe3ed39866e82d053c
TomaChen513 <xy_chen513@163.com>
2022-10-11 11:54:23 +0800

    feat(LeetCodeJava): add 1. Two Sum

--- LeetCodeJava/src/main/java/CodeImage/HashTable/TwoSum.java
+++ LeetCodeJava/src/main/java/CodeImage/HashTable/TwoSum.java
@@ -1,4 +1,22 @@
 package CodeImage.HashTable;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map=new HashMap<>();
+        int[] res=new int[2];
+        for (int i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+        for (int i=0;i< nums.length;i++){
+            if (map.containsKey(target-nums[i])&&i!=map.get(target-nums[i])){
+                res[0]=i;
+                res[1]=map.get(target-nums[i]);
+                return res;
+            }
+        }
+        return res;
+    }
 }
