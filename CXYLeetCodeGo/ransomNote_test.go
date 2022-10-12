717e6e112e93d85107fee6c13cc2a98e9119d3b4
TomaChen513 <xy_chen513@163.com>
2022-10-12 10:56:28 +0800

    feat(CXYGo):add 383. Ransom Note

---
+++ CXYLeetCodeGo/ransomNote_test.go
@@ -0,0 +1,25 @@
+package CXYLeetCodeGo
+
+import(
+	"testing"
+)
+
+func TestRansom(t *testing.T){
+	
+	t.Log(canConstruct("xcx","asd"))
+
+}
+
+func canConstruct(ransomNote string, magazine string) bool {
+	var chars [26]int
+	for i:=0;i<len(magazine);i++{
+		chars[magazine[i]-'a']++
+	}
+	for i:=0;i<len(ransomNote);i++{
+		chars[ransomNote[i]-'a']--;
+		if(chars[ransomNote[i]-'a']<0){
+			return false
+		}
+	}
+	return true
+}
