1b415d8a85c79ba8c94dfde04faec65f645929db
TomaChen513 <xy_chen513@163.com>
2022-10-26 16:48:19 +0800

    add 239,still need to think

--- CXYLeetCodeGo/DoubleElements/doubleE_test.go
+++ CXYLeetCodeGo/DoubleElements/doubleE_test.go
@@ -84,3 +84,62 @@ func replaceSpace(s string) string {
 	}
 	return string(b)
 }
+
+type MyQueue struct{
+	queue []int
+}
+
+func NewMyQueue() *MyQueue{
+	return &MyQueue{
+		queue: make([]int,0),
+	}
+}
+
+func (m *MyQueue) Front() int{
+	return m.queue[0];
+}
+
+func (m *MyQueue) Back() int{
+	return m.queue[len(m.queue)-1]
+}
+
+func (m *MyQueue) Empty() bool{
+	return len(m.queue)==0
+}
+
+func (m *MyQueue) Push(val int){
+	for !m.Empty() && m.Back()<val{
+		m.queue=m.queue[:len(m.queue)-1]
+	}
+	m.queue=append(m.queue,val)
+}
+
+func (m *MyQueue) Pop(val int){
+	if !m.Empty() && m.Front()==val{
+		m.queue=m.queue[1:]
+	}
+}
+
+
+
+// 239
+func maxSlidingWindow(nums []int, k int) []int {
+	queue:=NewMyQueue()
+	length:=len(nums)
+	res:=make([]int,0)
+
+	// 初始化部分
+	for i:=0;i<k;i++{
+		queue.Push(nums[i])
+	}
+
+	res=append(res,queue.Front())
+
+	for i:=k;i<length;i++{
+		queue.Pop(nums[i-k])
+		queue.Push(nums[i])
+		res=append(res,queue.Front())
+	}
+
+	return res
+}
