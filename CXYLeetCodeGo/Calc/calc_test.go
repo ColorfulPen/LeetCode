9461c1b8cee9f2bda0a2044a125403a4d7080799
TomaChen513 <xy_chen513@163.com>
2022-10-31 10:16:33 +0800

    update binary tree traversal

--- CXYLeetCodeGo/Calc/calc_test.go
+++ CXYLeetCodeGo/Calc/calc_test.go
@@ -1,23 +1,66 @@
 package calc
 
-import "testing"
-	
+import (
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	type args struct {
+		arg1 int
+		arg2 int
+	}
+	tests := []struct {
+		arg1 int
+		arg2 int
+		arg3 args
+	}{
+		{1, 2, args{1, 2}},
+	}
+	t.Log(tests[0].arg1)
+
+	t.Log(add(1, 3))
 }
 
-func TestAdd2(t *testing.T){
-	if ans :=add(1,2); ans!=3 {
-		t.Error("error")
+func Test_add(t *testing.T) {
+	type args struct {
+		num1 int
+		num2 int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"test1", args{1, 2}, 4},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := add(tt.args.num1, tt.args.num2); got != tt.want {
+				t.Errorf("add() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
-func TestAdd3(t *testing.T){
-	if ans :=add(1,2); ans!=3 {
-		t.Error("error")
+func TestGet(t *testing.T) {
+	type args struct {
+		index int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantRet int
+	}{
+		// TODO: Add test cases.
+		{"t1",args{2},3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotRet := Get(tt.args.index); gotRet != tt.wantRet {
+				t.Errorf("Get() = %v, want %v", gotRet, tt.wantRet)
+			}
+		})
 	}
 }
-func TestAdd4(t *testing.T){
-	if ans :=add(1,2); ans!=3 {
-		t.Error("error")
-	}
-}
-
