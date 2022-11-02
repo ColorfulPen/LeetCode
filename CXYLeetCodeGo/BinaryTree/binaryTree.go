87c558e8d1ceb037717891e5bbf52d61c0d1e603
TomaChen513 <xy_chen513@163.com>
2022-11-02 15:21:41 +0800

    binary tree

--- CXYLeetCodeGo/BinaryTree/binaryTree.go
+++ CXYLeetCodeGo/BinaryTree/binaryTree.go
@@ -1,6 +1,9 @@
 package binarytree
 
-import "container/list"
+import (
+	"container/list"
+	"math"
+)
 
 type TreeNode struct {
 	Val   int
@@ -79,23 +82,99 @@ func orderTraversalUnifiedIteration(root *TreeNode) (res []int) {
 	return res
 }
 
-func levelTraversal(root *TreeNode) (res []int){
-	if root==nil {
+func levelTraversal(root *TreeNode) (res []int) {
+	if root == nil {
 		return nil
 	}
-	queue:=list.New()
-	res=make([]int, 0)
+	queue := list.New()
+	res = make([]int, 0)
 	queue.PushBack(root)
-	for queue.Len()>0{
-		e:=queue.Front()
+	for queue.Len() > 0 {
+		e := queue.Front()
 		queue.Remove(e)
-		node:=e.Value.(*TreeNode)
+		node := e.Value.(*TreeNode)
 		res = append(res, node.Val)
-		if node.Left!=nil {
+		if node.Left != nil {
 			queue.PushBack(node.Left)
 		}
-		if node.Right!=nil {
+		if node.Right != nil {
 			queue.PushBack(node.Right)
+		}
+	}
+	return res
+}
+
+// 226
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
+	return root
+}
+
+// 101
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return isSym(root.Left, root.Right)
+}
+
+func isSym(leftNode *TreeNode, rightNode *TreeNode) bool {
+	if leftNode == nil && rightNode == nil {
+		return true
+	}
+	return leftNode != nil && rightNode != nil && leftNode.Val == rightNode.Val && isSym(leftNode.Left, rightNode.Right) && isSym(leftNode.Right, rightNode.Left)
+}
+
+// 104
+func maxDepth(root *TreeNode) int{
+	if root==nil {
+		return 0
+	}
+	leftHeight,rightHeight:=0,0
+	if root.Left!=nil {
+		leftHeight=maxDepth(root.Left)
+	}
+	if root.Right!=nil {
+		rightHeight=maxDepth(root.Right)
+	}
+	return int(math.Max(float64(leftHeight),float64(rightHeight)))+1
+}
+
+// 111
+func minDepth(root *TreeNode) int{
+	if root==nil {
+		return 0
+	}
+	if root.Left==nil {
+		return minDepth(root.Right)+1
+	}
+	if root.Right==nil {
+		return minDepth(root.Left)+1
+	}
+	return int(math.Min(float64(minDepth(root.Right)),float64(minDepth(root.Left))))+1
+}
+
+func countNodes(root *TreeNode) int{
+	if root==nil {
+		return 0
+	}
+	queue:=list.New()
+	queue.PushBack(root)
+	res:=0
+	for queue.Len()!=0 {
+		n:=queue.Len()
+		for i := 0; i < n; i++ {
+			node:=queue.Remove(queue.Front()).(*TreeNode)
+			if node.Left!=nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right!=nil {
+				queue.PushBack(node.Right)
+			}
+			res++
 		}
 	}
 	return res
--- CXYLeetCodeGo/BinaryTree/binaryTree_test.go
+++ CXYLeetCodeGo/BinaryTree/binaryTree_test.go
@@ -1,6 +1,1 @@
 package binarytree
-
-import (
-	"testing"
-)
-
