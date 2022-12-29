e49076940c16d1f2ee3bc263fb433b7a9e1a0c7b
TomaChen513 <xy_chen513@163.com>
2022-12-29 17:49:34 +0800

    12.22

--- TomaCritical/9DynamicPrograming/dynamicProgram.go
+++ TomaCritical/9DynamicPrograming/dynamicProgram.go
@@ -52,3 +52,23 @@ func min(a,b int)int{
 	}
 	return b
 }
+
+// 62. 不同路径  63. 不同路径 II
+func uniquePaths(m int, n int) int {
+	dp:=make([][]int,m)
+	for i := 0; i < m; i++ {
+		dp[i]=make([]int, n)
+	}
+	for i := 0; i < m; i++ {
+		dp[i][0]=1
+	}
+	for i := 0; i < n; i++ {
+		dp[0][i]=1
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j]=dp[i][j-1]+dp[i-1][j]
+		}
+	}
+	return dp[m-1][n-1]
+}
