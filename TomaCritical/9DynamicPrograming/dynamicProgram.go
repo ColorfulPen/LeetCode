332f45d1e295ec96cb9ebeb216cecaa477f814c6
TomaChen513 <xy_chen513@163.com>
2022-12-30 12:31:16 +0800

    12.24

--- TomaCritical/9DynamicPrograming/dynamicProgram.go
+++ TomaCritical/9DynamicPrograming/dynamicProgram.go
@@ -101,3 +101,20 @@ func numTrees(n int) int {
 	}
 	return dp[n]
 }
+
+// 300. 最长递增子序列
+func lengthOfLIS(nums []int) int {
+	dp := make([]int, len(nums))
+	maxNum:=0
+	
+	for i := 1; i < len(nums); i++ {
+		for j := 0; j < i; j++ {
+			if nums[i]>nums[j] {
+				dp[i]=max(dp[i],dp[j]+1)
+			}
+		}
+		maxNum=max(maxNum,dp[i])
+	}
+	return maxNum+1
+}
+
