18e3da7a153f985b6feef88e0b5ea48d10af4f47
TomaChen513 <xy_chen513@163.com>
2023-01-04 18:00:21 +0800

    12.31

--- TomaCritical/6BinaryTree/binaryTree.go
+++ TomaCritical/6BinaryTree/binaryTree.go
@@ -295,3 +295,30 @@ func constructMaximumBinaryTree(nums []int) *TreeNode {
 	}
 	return buildMaxBinaryTree(nums)
 }
+
+// 617. 合并二叉树
+func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	if root1==nil {
+		return root2
+	}
+	if root2==nil {
+		return root1
+	}
+	root1.Val+=root2.Val
+	root1.Left=mergeTrees(root1.Left,root2.Left)
+	root1.Right=mergeTrees(root1.Right,root2.Right)
+	return root1
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root==nil {
+		return nil
+	}
+	if root.Val==val {
+		return root
+	}else if root.Val>val {
+		return searchBST(root.Left,val)
+	}else {
+		return searchBST(root.Right,val)
+	}
+}
